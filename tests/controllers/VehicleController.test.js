fc769ebf83df30641b21bd19cff9ebbb383566f1
arbexmb <lucasarbexmb@gmail.com>
2020-02-27 17:09:29 -0300

    Minor changes.

--- tests/controllers/VehicleController.test.js
+++ tests/controllers/VehicleController.test.js
@@ -12,23 +12,31 @@ describe('VehicleController', () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(process.env.DB_TEST);
         db = await connection.db(process.env.DBNAME_TEST);
     });
 
     afterAll(async() => {
+        await db.dropDatabase();
         await connection.close();
         await db.close();
     });
 
     const mockVehicle = {
+        _id: 12345,
         vehicle: "Ecosport",
         brand: "Ford",
         year: 2015,
         description: "Lorem ipsum dolor."
     };
 
-    test('expect store on VehicleController to create a vehicle', () => {
+    test('expect a vehicle can be created', async () => {
+        const vehicles = db.collection('vehicles');
+        await vehicles.insertOne(mockVehicle);
+        const insertedVehicle = await vehicles.findOne({ _id: 12345 });
+        expect(insertedVehicle).toEqual(mockVehicle);
+    });
+
+    test('expect store method on VehicleController to add a vehicle on collection', async () => {
         return request(app).post('/vehicles').send(mockVehicle).then((res) => {
             expect(res.status).toBe(201);
             expect(res.body.vehicle).toBe("Ecosport");
